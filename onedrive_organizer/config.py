ea69bb4dd8ef53288a722ff80b0ed74c7e492c54
Simon Dudanski <simon@dudanski.de>
2025-02-23 00:13:05 +0100

    sync database to onedirce

--- onedrive_organizer/config.py
+++ onedrive_organizer/config.py
@@ -9,5 +9,10 @@ TENANT_ID = os.getenv("TENANT_ID", "common")
 SCOPES = ["Files.ReadWrite"]
 AUTHORITY = f"https://login.microsoftonline.com/{TENANT_ID}"
 GRAPH_API_URL = "https://graph.microsoft.com/v1.0/me/drive"
-LOCAL_DB_FILE = "files_metadata.db"  # Lokale SQLite-Datei
+
+# Lokale und OneDrive-Pfade für die SQLite-Datenbank
+LOCAL_DB_FILE = "files_metadata.db"  # Lokale Kopie
 ONEDRIVE_DB_PATH = "/files_metadata.db"  # Zielpfad in OneDrive
+
+# Token-Datei für Authentifizierung
+TOKEN_FILE = "token.json"
