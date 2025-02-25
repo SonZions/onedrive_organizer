b3d17469510ddad7794bb347c638b8fcc5b0714e
SonZions <96846796+SonZions@users.noreply.github.com>
2025-02-25 19:47:02 +0100

    fix setting

--- dms_ui/settings.py
+++ dms_ui/settings.py
@@ -57,10 +57,10 @@ MIDDLEWARE = [
 ROOT_URLCONF = 'dms_ui.urls'
 
 
+from pathlib import Path
 
-import os
+BASE_DIR = Path(__file__).resolve().parent.parent
 
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 
 TEMPLATES = [
@@ -84,11 +84,13 @@ WSGI_APPLICATION = 'dms_ui.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
+from onedrive_organizer.config import DB_PATH
+
 
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': str(DB_PATH),
     }
 }
 
